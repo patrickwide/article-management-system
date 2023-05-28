06f444fb84757278ead251339ada94e099303e86
patrickwide <patrickwide254@gmail.com>
2023-05-28 10:45:25 +0300

    Add methods to retrieve author's articles and magazines

--- author.rb
+++ author.rb
@@ -4,4 +4,12 @@ class Author
     def initialize(name)
       @name = name
     end
+
+    def articles
+        Article.all.select { |article| article.author == self }
+      end
+    
+      def magazines
+        articles.map { |article| article.magazine }.uniq
+    end
 end
