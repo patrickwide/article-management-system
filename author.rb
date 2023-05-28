56d7aa087c231d79cf0e1543d355bc3bf65500ad
patrickwide <patrickwide254@gmail.com>
2023-05-28 10:41:19 +0300

    Implement Magazine class

--- author.rb
+++ author.rb
@@ -4,20 +4,4 @@ class Author
     def initialize(name)
       @name = name
     end
-  
-    def articles
-      Article.all.select { |article| article.author == self }
-    end
-  
-    def magazines
-      articles.map { |article| article.magazine }.uniq
-    end
-  
-    def add_article(magazine, title)
-      Article.new(self, magazine, title)
-    end
-  
-    def topic_areas
-      magazines.map { |magazine| magazine.category }.uniq
-    end
 end
---
+++ magazine.rb
@@ -0,0 +1,9 @@
+class Magazine
+    attr_accessor :name, :category
+  
+    def initialize(name, category)
+      @name = name
+      @category = category
+    end
+end
+  
