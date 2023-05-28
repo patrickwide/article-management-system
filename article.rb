9c3added0c9c6f68dd225abb20dd9352874906ef
patrickwide <patrickwide254@gmail.com>
2023-05-28 10:42:16 +0300

    Implement Article class

---
+++ article.rb
@@ -0,0 +1,10 @@
+class Article
+    attr_reader :author, :magazine, :title
+  
+    def initialize(author, magazine, title)
+      @author = author
+      @magazine = magazine
+      @title = title
+    end
+end
+  
